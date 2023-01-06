6c773d3a3c0a7e3fbba7823c1b7f3cee088b48c9
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-06 13:05:24 +0600

    server start

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
 const port = process.env.PORT || 5000;
+
 const app = express();
 
 // middleware
-app.use(cors());
-app.use(express.json());
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ohmmz47.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-
-async function run() {
-  try {
-    
-  }
-  finally {
-    
-  }
-}
-run( ).catch(console.dir)
+app.use(cors())
+app.use(express.json())
 
 app.get(`/`, (req, res) => {
-  res.send(`Jhon is running and waiting for ema`);
-});
+  res.send(`John is running and waiting for ema`)
+})
 
 app.listen(port, () => {
-  console.log(`Jhon is running on port `, port);
-});
+  console.log(`John is running on port`, port);
+})
